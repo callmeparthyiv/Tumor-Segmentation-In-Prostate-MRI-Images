c6dadb902d1d96675a1ca370ff838b2f4ae4c62e
Parth Shanishchara <shanishcharaparth@gmail.com>
2025-02-11 18:01:08 -0600

    main.py for training and testing

---
+++ main.py
@@ -0,0 +1,9 @@
+import glob
+import numpy as np
+from data_preprocessing import read_data
+
+#path of dataset
+train_path = 'resized_preprocessed_data/train_dataset/'
+test_path  = 'resized_preprocessed_data/test_dataset/'
+
+ 
