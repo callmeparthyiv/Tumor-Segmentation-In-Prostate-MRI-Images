3db0cfba6dd1cc683a0f3922c48105ae1f55aca3
Parth Shanishchara <shanishcharaparth@gmail.com>
2025-02-12 23:56:17 -0600

    checking model building

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import glob
 import numpy as np
 from data_preprocessing import read_data
+from model import build_model, conv_block, encoder_block, decoder_block
 import SimpleITK as sitk
+from keras.optimizers import Adam
+
 #path of dataset
 train_path = 'resized_preprocessed_dataset/training_data/'
 test_path  = 'resized_preprocessed_dataset/test_data/'
@@ -22,3 +25,12 @@ path_of_train_mask, path_of_train_data = read_data(train_path)
 train_data = read_img_volume(path_of_train_data)
 t_data = train_data[0]
 
+BATCH = 64
+IMG_DEPTH = t_data.shape[0]
+IMG_HEIGHT = t_data.shape[1]
+IMG_WIDTH = t_data.shape[2]
+
+input_shape = (BATCH, IMG_DEPTH, IMG_HEIGHT, IMG_WIDTH, )
+model = build_model(input_shape)
+model.compile(optimizer=Adam(lr = 1e-3), loss = "binary_crossentropy", metrics = ["accuracy"] )
+model.summary()
