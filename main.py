76bd1e98c0c0f4dc04b10b0f017f7bd8b4da91d3
Parth Shanishchara <shanishcharaparth@gmail.com>
2025-02-17 23:28:03 -0600

    model training starts

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import glob
 import numpy as np
 from data_preprocessing import read_data
-from model import build_model, conv_block, encoder_block, decoder_block
+from model import build_model
 import SimpleITK as sitk
+
+import os
+os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+
+import tensorflow as tf
 from keras.optimizers import Adam
+
+from keras import backend as K
+K.clear_session()
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+
 
 #path of dataset
 train_path = 'resized_preprocessed_dataset/training_data/'
@@ -23,14 +38,23 @@ def read_img_volume(list_of_image_path):
 path_of_train_mask, path_of_train_data = read_data(train_path)
 
 train_data = read_img_volume(path_of_train_data)
-t_data = train_data[0]
+segmentation_data = read_img_volume(path_of_train_mask)
 
-BATCH = 64
-IMG_DEPTH = t_data.shape[0]
-IMG_HEIGHT = t_data.shape[1]
-IMG_WIDTH = t_data.shape[2]
 
-input_shape = (BATCH, IMG_DEPTH, IMG_HEIGHT, IMG_WIDTH, )
+IMG_DEPTH = 28
+IMG_HEIGHT = 320
+IMG_WIDTH = 320
+
+
+input_shape = (IMG_DEPTH, IMG_HEIGHT, IMG_WIDTH, 1)
 model = build_model(input_shape)
-model.compile(optimizer=Adam(lr = 1e-3), loss = "binary_crossentropy", metrics = ["accuracy"] )
-model.summary()
+
+
+
+
+model.compile(optimizer='Adam', loss = "binary_crossentropy", metrics = ["accuracy"] )
+
+print(model.summary())
+
+
+history = model.fit(train_data, segmentation_data, epochs=10, verbose = 1, batch_size=2)
