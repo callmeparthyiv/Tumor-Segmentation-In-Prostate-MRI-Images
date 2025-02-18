be74f9b52c81d1db63e12b884b3b2732885ad36b
Parth Shanishchara <shanishcharaparth@gmail.com>
2025-02-17 20:47:07 -0600

    model with just two layers

--- model.py
+++ model.py
@@ -3,26 +3,31 @@ from keras.layers import Input, Conv3D, MaxPool3D, Conv3DTranspose, concatenate,
 
 #convolution block
 def conv_block(input, num_filters):
-    s = Conv3D(num_filters, 3, padding="same")(input)
+    s = Conv3D(num_filters, (3,3,3), padding="same")(input)
     s = BatchNormalization()(s)
     s = Activation("relu")(s)
+    print("Conv1: ",s.shape)
     
-    s = Conv3D(num_filters, 3, padding="same")(s)
+    s = Conv3D(num_filters, (3,3,3), padding="same")(s)
     s = BatchNormalization()(s)
     s = Activation("relu")(s)
-    
+    print("Conv2: ",s.shape)
     return s
 
 #encoder block
 def encoder_block(input, num_filters):
+    print("Encoder1: ",input.shape)
     s = conv_block(input, num_filters)
     p = MaxPool3D((2,2,2))(s)
-    
+    print("MaxPooling: ", p.shape)
     return s, p
 
 #decoder block
 def decoder_block(input, skip_features, num_filters):
-    s = Conv3DTranspose(num_filters, (3,3), strides=3, padding="same")(input)
+    print("Decoder: ",input.shape)
+    print("Skip_features Shape", skip_features.shape)
+    s = Conv3DTranspose(num_filters, 2, strides=2, padding="same")(input)
+    print("Concatenation: ",s.shape)
     s = Concatenate()([s, skip_features])
     s = conv_block(s, num_filters)
     
@@ -32,20 +37,16 @@ def decoder_block(input, skip_features, num_filters):
 def build_model(input_shape):
    
     input = Input(input_shape)
-   
-    s1, p1 = encoder_block(input, 64)
-    s2, p2 = encoder_block(p1, 128)
-    s3, p3 = encoder_block(p2, 256)
-    s4, p4 = encoder_block(p3, 512)
+    print("input: ",input.shape)
+    s1, p1 = encoder_block(input, 128)
+    s2, p2 = encoder_block(p1, 256)
     
-    b1 = conv_block(p4, 1024)
+    b1 = conv_block(p2, 512)
     
-    d1 = decoder_block(b1, s4, 512)
-    d2 = decoder_block(d1, s3, 256)
-    d3 = decoder_block(d2, s2, 128)
-    d4 = decoder_block(d3, s1, 64)
+    d1 = decoder_block(b1, s2, 256)
+    d2 = decoder_block(d1, s1, 128)
     
-    output = Conv3D(1, 1, padding="same", activation="sigmoid")(d4)
+    output = Conv3D(1, 1, padding="same", activation="sigmoid")(d2)
     model = Model(input, output, name = "U-net")
     
     return model
