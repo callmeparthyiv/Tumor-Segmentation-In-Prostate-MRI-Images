8bf6e340d53a25954658e51f3d66c875107a8fa0
Parth Shanishchara <shanishcharaparth@gmail.com>
2025-04-22 16:20:37 -0600

    model architecture completed

--- model.py
+++ model.py
@@ -1,52 +1,40 @@
 from keras.models import Model
-from keras.layers import Input, Conv3D, MaxPool3D, Conv3DTranspose, concatenate, Concatenate, BatchNormalization, Activation 
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, UpSampling3D, Conv3DTranspose, concatenate, Concatenate, BatchNormalization, Activation
+from tensorflow.keras.regularizers import  l2
 
 #convolution block
 def conv_block(input, num_filters):
-    s = Conv3D(num_filters, (3,3,3), padding="same")(input)
-    s = BatchNormalization()(s)
-    s = Activation("relu")(s)
-    print("Conv1: ",s.shape)
-    
-    s = Conv3D(num_filters, (3,3,3), padding="same")(s)
-    s = BatchNormalization()(s)
-    s = Activation("relu")(s)
-    print("Conv2: ",s.shape)
+    s = Conv3D(num_filters, (5,5,5),activation='relu', kernel_regularizer=l2(0.001), padding="same")(input)
+    s = Conv3D(num_filters, (5,5,5),activation='relu', kernel_regularizer=l2(0.001), padding="same")(s)
     return s
 
 #encoder block
 def encoder_block(input, num_filters):
-    print("Encoder1: ",input.shape)
     s = conv_block(input, num_filters)
-    p = MaxPool3D((2,2,2))(s)
-    print("MaxPooling: ", p.shape)
+    p = MaxPooling3D(pool_size=(2,2,2))(s)
     return s, p
 
 #decoder block
-def decoder_block(input, skip_features, num_filters):
-    print("Decoder: ",input.shape)
-    print("Skip_features Shape", skip_features.shape)
-    s = Conv3DTranspose(num_filters, 2, strides=2, padding="same")(input)
-    print("Concatenation: ",s.shape)
-    s = Concatenate()([s, skip_features])
+def decoder_block(input, skip_features, num_filters):  
+    s = UpSampling3D(size = (2,2,2))(input)
+    s = Conv3D(num_filters, 2, padding='same', kernel_regularizer=l2(0.001), activation='relu')(s)
+    s = concatenate([s, skip_features], axis=-1)
     s = conv_block(s, num_filters)
-    
     return s
 
 #model building
 def build_model(input_shape):
    
     input = Input(input_shape)
-    print("input: ",input.shape)
     s1, p1 = encoder_block(input, 32)
     s2, p2 = encoder_block(p1, 64)
-    
+
     b1 = conv_block(p2, 128)
-    
+
     d1 = decoder_block(b1, s2, 64)
     d2 = decoder_block(d1, s1, 32)
-    
-    output = Conv3D(1, 1, padding="same", activation="sigmoid")(d2)
+
+    output = Conv3D(1, 1, kernel_regularizer=l2(0.001), activation="sigmoid")(d2)
     model = Model(input, output, name = "U-net")
     
     return model
